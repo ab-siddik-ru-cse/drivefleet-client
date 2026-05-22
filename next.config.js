72f947ea489808c00d16513751907593e52db0df
ab-siddik-ru-cse <mdabubokkor1024@gmail.com>
2026-05-22 17:17:02 +0600

    image optimaization added

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true,
-    remotePatterns: [{ protocol: "https", hostname: "**" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
